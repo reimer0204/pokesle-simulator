ebd4f7300d8a4fa11c4ecc8bfd7cd8cdb35beb7b
reimer0204 <reimer0204@gmail.com>
2024-09-06 00:34:54 +0900

    スイクン追加

--- src/models/evaluate-table.js
+++ src/models/evaluate-table.js
@@ -8,7 +8,7 @@ import MultiWorker from "./multi-worker";
 
 export default class EvaluateTable {
 
-  static VERSION = 20240824;
+  static VERSION = 20240903;
 
   static load(config) {
     if (config.version.evaluateTable != this.VERSION) {
