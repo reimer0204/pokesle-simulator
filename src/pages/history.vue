912901b88f18b10ea3ba7fe3c95258026765979f
晴空リウル <reimer0204@yahoo.co.jp>
2026-05-21 20:24:19 +0900

    プクリン更新

--- src/pages/history.vue
+++ src/pages/history.vue
@@ -22,7 +22,17 @@ config.version.history = Version.HISTORY;
     </div>
 
     <h2 class="mt-1em">更新履歴</h2>
-    
+
+    <h3>2026/5/21</h3>
+    <ul>
+      <li>
+        機能改善
+        <ul>
+          <li>プクリンの調整を反映</li>
+        </ul>
+      </li>
+    </ul>
+
     <h3>2026/5/20</h3>
     <ul>
       <li>
